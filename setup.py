c96bda0a6fc09f65333502a2300e8226f9374a3c
M69k65y <themichael.01.wanyoike@gmail.com>
2018-12-10 18:36:56 +0300

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+	name = "flask-simple-validator",
+	version = "1.0",
+	description = "A simple JSON validator for use in Python projects. (Built in Python 3)",
+	url = "https://github.com/M69k65y/json-simple-validator",
+	author = "M69k65y",
+	license = "MIT",
+	packages = ["json-simple-validator"],
+	zip_safe=False
+)
